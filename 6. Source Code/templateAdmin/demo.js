d3e7cc01bc9a45a76211e1c78883728056f57367
ronnievn <ronnievn@37c50502-1b00-b293-b119-f43cca8460b1>
2010-05-02 18:24:22 +0000

    Nhom Code 13. Tiep theo la Template cho Admin. Nhom se update ngay khi cac module duoc code xong

---
+++ 6. Source Code/templateAdmin/demo.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+		
+	
+	
+	// third example
+	$("#red").treeview({
+		animated: "slow",
+		collapsed: true,
+		unique: true,
+		persist: "cookie",
+		toggle: function() {
+			window.console && console.log("%o was toggled", this);
+		}
+	});
+	
+	// third example
+	$("#blue").treeview({
+		animated: "slow",
+		collapsed: true,
+		unique: true,
+		persist: "cookie",
+		toggle: function() {
+			window.console && console.log("%o was toggled", this);
+		}
+	});
+
+
+});
