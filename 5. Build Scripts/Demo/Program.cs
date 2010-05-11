deac115d12d921db23c1d78589da6062b4b6ca72
conmasieuquay <conmasieuquay@37c50502-1b00-b293-b119-f43cca8460b1>
2010-05-11 11:47:59 +0000

    demo build script ung dung console cong 2 so. de nghi nhom code lien he de tao lai solution phuc vu cho viec viet build script do an. Nhom 12.

---
+++ 5. Build Scripts/Demo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 6;
+            int b = 7;
+            TinhToan tt = new TinhToan();
+
+            int kq = tt.Tong(a, b);
+
+            Console.WriteLine(a.ToString() + " + " + b.ToString() + " = " + kq.ToString());
+
+        }
+    }
+}
---
+++ 5. Build Scripts/Demo/TinhToan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+   public class TinhToan
+    {
+        public int Tong(int a, int b)
+        {
+            return (a + b);
+        }
+    }
+}
