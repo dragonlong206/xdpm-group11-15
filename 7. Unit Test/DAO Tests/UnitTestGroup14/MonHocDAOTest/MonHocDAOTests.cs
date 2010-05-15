114f1b8085ff33f4c6055fe76a925d9dd4819b73
conguyetnhat <conguyetnhat@37c50502-1b00-b293-b119-f43cca8460b1>
2010-05-15 19:57:30 +0000

    sửa mon hoc

--- 7. Unit Test/DAO Tests/UnitTestGroup14/MonHocDAOTest/MonHocDAOTests.cs
+++ 7. Unit Test/DAO Tests/UnitTestGroup14/MonHocDAOTest/MonHocDAOTests.cs
@@ -3,113 +3,74 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
-using System.Data.SqlClient;
-using Dto;
 
 namespace UnitTestGroup14
 {
-        [TestFixture]
-        public class MonHoc
+    [TestFixture]
+    public class MonHocDAOTests
+    {
+
+        [Test]
+        public void insertMonHocTest()
         {
-            SqlConnection cn;
+            //TODO Write this test
 
-            [SetUp]
-            public void iNit()
-            {
-                cn = new SqlConnection();
-                cn.Open();
-            }
-            [TearDown]
-            public void cleanup()
-            {
-                cn.Close();
-            }
-
-            [Test]
-            public void insertLoaiAccTest()
-            {
-                //TODO Write this test
-               // Assert.That(LoaiAccDAOTestRewrite.insertLoaiAcc(new LoaiAccDTO()), Is.True);
-
-            }
-
-            [Test]
-            public void deleteLoaiAccTest()
-            {
-                //TODO Write this test
-                //LoaiAccDTO obj = new LoaiAccDTO();
-               // obj.IDAcc = "obj";
-                //Assert.That(LoaiAccDAOTestRewrite.deleteLoaiAcc(obj.IDAcc),Is.True);
-
-            }
-
-            [Test]
-            public void updateLoaiAccTest()
-            {
-                //TODO Write this test
-              //  Assert.That(LoaiAccDAOTestRewrite.updateLoaiAcc(new LoaiAccDTO()), Is.True);
-
-            }
-
-            [Test]
-            public void selectAllLoaiAccTest()
-            {
-                //TODO Write this test
-                //Assert.That(LoaiAccDAOTestRewrite.selectAllLoaiAcc(), Is.True);
-
-            }
-
-            [Test]
-            public void selectLoaiAccByLACTest()
-            {
-                //TODO Write this test
-               // LoaiAccDTO obj = new LoaiAccDTO();
-               // obj.IDLoaiAcc = 1 ;
-             //   Assert.That(LoaiAccDAOTestRewrite.selectLoaiAccByLAC(obj.IDLoaiAcc), Is.True);
-
-            }
-
-            [Test]
-            public void selectLoaiAccByIDACTest()
-            {
-                //TODO Write this test
-
-            }
-
-            [Test]
-            public void selectLoaiAccByEmailTest()
-            {
-                //TODO Write this test
-
-            }
-
-            [Test]
-            public void ToStringTest()
-            {
-                //TODO Write this test
-
-            }
-
-            [Test]
-            public void EqualsTest()
-            {
-                //TODO Write this test
-
-            }
-
-            [Test]
-            public void GetHashCodeTest()
-            {
-                //TODO Write this test
-
-            }
-
-            [Test]
-            public void GetTypeTest()
-            {
-                //TODO Write this test
-
-            }
         }
-    
+
+        [Test]
+        public void deleteMonHocTest()
+        {
+            //TODO Write this test
+
+        }
+
+        [Test]
+        public void updateMonHocTest()
+        {
+            //TODO Write this test
+
+        }
+
+        [Test]
+        public void selectAllMonHocTest()
+        {
+            //TODO Write this test
+
+        }
+
+        [Test]
+        public void selectMonHocByIDMonHocTest()
+        {
+            //TODO Write this test
+
+        }
+
+        [Test]
+        public void ToStringTest()
+        {
+            //TODO Write this test
+
+        }
+
+        [Test]
+        public void EqualsTest()
+        {
+            //TODO Write this test
+
+        }
+
+        [Test]
+        public void GetHashCodeTest()
+        {
+            //TODO Write this test
+
+        }
+
+        [Test]
+        public void GetTypeTest()
+        {
+            //TODO Write this test
+
+        }
+    }	
 }
