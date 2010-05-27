f190d2c8a5bb1cdc399ac586e9755c2e4ab72a98
gtlong.hcmus <gtlong.hcmus@37c50502-1b00-b293-b119-f43cca8460b1>
2010-05-27 20:36:47 +0000

    UnitTest cho LoaiAccDAO

--- 7. Unit Test/DAO Tests/UnitTestGroup14/LoaiAccDAOTest/LoaiAccDAOTests.cs
+++ 7. Unit Test/DAO Tests/UnitTestGroup14/LoaiAccDAOTest/LoaiAccDAOTests.cs
@@ -3,74 +3,83 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
+using NUnit.Mocks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace UnitTestGroup14
 {
     [TestFixture]
     public class LoaiAccDAOTests
     {
+        LoaiAccDAO _LoaiAccDAO;
+        ISqlDataAccessHelper _sqlDataAccessHelper;
+        DynamicMock _controller;
+
 
         [Test]
         public void insertLoaiAccTest()
         {
+            _controller = new DynamicMock(typeof(ISqlDataAccessHelper));
+            _sqlDataAccessHelper = _controller.MockInstance as ISqlDataAccessHelper;
+            _LoaiAccDAO = new LoaiAccDAO(_sqlDataAccessHelper);
             //TODO Write this test
 
+
+            _controller.ExpectAndReturn("ExecuteNonQuery",1,string.Empty,new List<SqlParameter>());
+            Assert.That(_LoaiAccDAO.insertLoaiAcc(new Dto.LoaiAccDTO()),Is.True);
         }
 
         [Test]
         public void deleteLoaiAccTest()
         {
+            _controller = new DynamicMock(typeof(ISqlDataAccessHelper));
+            _sqlDataAccessHelper = _controller.MockInstance as ISqlDataAccessHelper;
+            _LoaiAccDAO = new LoaiAccDAO(_sqlDataAccessHelper);
             //TODO Write this test
 
+
+            _controller.ExpectAndReturn("ExecuteNonQuery",0,string.Empty,new List<SqlParameter>());
+            Assert.That(_LoaiAccDAO.deleteLoaiAcc(0),Is.False);
         }
 
         [Test]
         public void updateLoaiAccTest()
         {
+            _controller = new DynamicMock(typeof(ISqlDataAccessHelper));
+            _sqlDataAccessHelper = _controller.MockInstance as ISqlDataAccessHelper;
+            _LoaiAccDAO = new LoaiAccDAO(_sqlDataAccessHelper);
             //TODO Write this test
 
+
+            _controller.ExpectAndReturn("ExecuteNonQuery",0,string.Empty,new List<SqlParameter>());
+            Assert.That(_LoaiAccDAO.updateLoaiAcc(new Dto.LoaiAccDTO()),Is.False);
         }
 
         [Test]
         public void selectAllLoaiAccTest()
         {
+            _controller = new DynamicMock(typeof(ISqlDataAccessHelper));
+            _sqlDataAccessHelper = _controller.MockInstance as ISqlDataAccessHelper;
+            _LoaiAccDAO = new LoaiAccDAO(_sqlDataAccessHelper);
             //TODO Write this test
 
+
+            _controller.ExpectAndReturn("ExecuteQuery",null,string.Empty);
+            Assert.That(_LoaiAccDAO.selectAllLoaiAcc(),Is.Null);
         }
 
         [Test]
         public void selectLoaiAccByIDLoaiAccTest()
         {
+            _controller = new DynamicMock(typeof(ISqlDataAccessHelper));
+            _sqlDataAccessHelper = _controller.MockInstance as ISqlDataAccessHelper;
+            _LoaiAccDAO = new LoaiAccDAO(_sqlDataAccessHelper);
             //TODO Write this test
 
-        }
 
-        [Test]
-        public void ToStringTest()
-        {
-            //TODO Write this test
-
-        }
-
-        [Test]
-        public void EqualsTest()
-        {
-            //TODO Write this test
-
-        }
-
-        [Test]
-        public void GetHashCodeTest()
-        {
-            //TODO Write this test
-
-        }
-
-        [Test]
-        public void GetTypeTest()
-        {
-            //TODO Write this test
-
-        }
+            _controller.ExpectAndReturn("ExecuteQuery",null,string.Empty,new List<SqlParameter>());
+            Assert.That(_LoaiAccDAO.selectLoaiAccByIDLoaiAcc(0),Is.Null);
+        }				
     }
 }
